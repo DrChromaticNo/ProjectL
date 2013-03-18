443ac9a7e663c3fb2302a9a70edcf3bc0380be00
Chris <ckerich@gmail.com>
2013-03-18 19:44:12 -0400

    added some javadoc to Deck
    realized I need to do a lot more javadoc

--- ProjectL/src/cards/Deck.java
+++ ProjectL/src/cards/Deck.java
@@ -9,6 +9,18 @@ import java.awt.Color;
 
 public interface Deck {
 
+	/**
+	 * Given a valid faction and card value, returns the silver number of that card
+	 * @param faction the Card's faction
+	 * @param value the card's value
+	 * @return The silver number of that card, according to this deck
+	 */
 	public int getSilverNum(Color faction, int value);
+	
+	/**
+	 * To get the list of all the valid card numbers for this deck
+	 * @return The list of all cards in this deck in an array (sorted in order?)
+	 */
+	public int[] allCards();
 
 }
