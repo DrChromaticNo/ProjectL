26dbb7fa6f1276d348f2b0143d99dd406a83c856
Chris <ckerich@gmail.com>
2013-03-19 17:51:23 -0400

    added treasurebag interface

---
+++ ProjectL/src/score/TreasureBag.java
@@ -0,0 +1,23 @@
+package score;
+
+public interface TreasureBag {
+
+	/**
+	 * removes a random treasure from the bag and returns it
+	 * @return a string corresponding to a treasure
+	 */
+	public String RandomTreasure();
+	
+	/**
+	 * Changes the amt of treasure by the given value
+	 * @param treasure the treasure whose amount is changing
+	 * @param value the amount it's being changed by
+	 */
+	public void modBag(String treasure, int value);
+	
+	/**
+	 * Resets the bag to its starting values
+	 */
+	public void resetBag();
+	
+}
