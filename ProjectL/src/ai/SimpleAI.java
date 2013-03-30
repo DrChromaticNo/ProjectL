74644dc8fda9806f9e213f709ef84c6fea522650
Chris <ckerich@gmail.com>
2013-03-29 23:05:20 -0400

    implemented the simpleAI class

---
+++ ProjectL/src/ai/SimpleAI.java
@@ -0,0 +1,35 @@
+package ai;
+
+import java.util.Random;
+
+import main.GameState;
+import players.Player;
+import score.ScoreCounter;
+import score.TreasureBag;
+import cards.Card;
+import cards.Deck;
+
+/**
+ * This class randomly chooses a course of action to take
+ * @author Chris
+ *
+ */
+public class SimpleAI implements AI {
+
+	@Override
+	public GameState choose(Player player, GameState[] states, Deck deck,
+			TreasureBag bag, ScoreCounter counter) {
+		
+		Random choice = new Random();
+		return states[choice.nextInt(states.length)];
+	}
+
+	@Override
+	public Card chooseCard(Player player, Card[] cards, GameState state,
+			Deck deck) {
+		
+		Random choice = new Random();
+		return cards[choice.nextInt(cards.length)];
+	}
+
+}
