51c168adf5fc9400655a88b5fe5ac562a95d1660
Chris <ckerich@gmail.com>
2013-04-07 22:59:38 -0400

    modified preacher text again

--- ProjectL/src/standard/Preacher.java
+++ ProjectL/src/standard/Preacher.java
@@ -84,8 +84,8 @@ public class Preacher implements Action {
 				
 				if(allEmpty)
 				{
-					System.out.println("\n" + "The preacher" + card.abbreviate() + 
-							" had no treasures to choose from\n");
+					System.out.println("\n" + "The preacher (" + card.abbreviate() + 
+							") had no treasures to choose from\n");
 					return state;
 				}
 				
@@ -95,8 +95,8 @@ public class Preacher implements Action {
 				
 				if(bag.countTreasure(treasure) != 0) //if we chose a valid treasure, remove all others and set it to 1
 				{	
-					System.out.println("\nThe preacher" + card.abbreviate() + 
-							" discarded all treasures besides 1 " + treasure + "\n");
+					System.out.println("\nThe preacher (" + card.abbreviate() + 
+							") discarded all treasures besides 1 " + treasure + "\n");
 					for(String s : Treasure.allTreasures())
 					{
 						if(s.equals(treasure))
@@ -139,8 +139,8 @@ public class Preacher implements Action {
 		
 		if(allEmpty)
 		{
-			choiceMap.put(state, "\n" + "The preacher" + card.abbreviate() + 
-					" had no treasures to choose from\n");
+			choiceMap.put(state, "\n" + "The preacher (" + card.abbreviate() + 
+					") had no treasures to choose from\n");
 			return choiceMap;
 		}
 		
@@ -162,8 +162,8 @@ public class Preacher implements Action {
 							.addLoot(s, -state.getPlayer(faction).getLoot().countTreasure(s));
 					}
 				}
-				choiceMap.put(tempState, "\nThe preacher" + card.abbreviate() + 
-						" discarded all treasures besides 1 " + treasure + "\n");
+				choiceMap.put(tempState, "\nThe preacher (" + card.abbreviate() + 
+						") discarded all treasures besides 1 " + treasure + "\n");
 			}
 		}
 		
