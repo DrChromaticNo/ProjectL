69e0c97d4f6687b424f07e7ca943f0c5a214463d
Chris <ckerich@gmail.com>
2013-12-21 20:11:02 -0500

    networking test works with multiple clients

--- ProjectL/src/test/NetworkingTester.java
+++ ProjectL/src/test/NetworkingTester.java
@@ -14,6 +14,7 @@ import networking.NetworkGUI;
 
 import ai.SimpleAI;
 
+import players.Faction;
 import players.Player;
 import standard.StandardScoreCounter;
 import standard.StandardSettings;
@@ -24,11 +25,14 @@ public class NetworkingTester {
 	@Test
 	public void networkTest1()
 	{
-		Player p1 = new Player(Color.WHITE, new SimpleAI());
-		Player p2 = new Player(Color.BLACK, new NetworkGUI(11100));
-		Player[] list = {p1, p2};
+		Player p1 = new Player(Faction.WHITE, new SimpleAI());
+		Player p2 = new Player(Faction.BLACK, new NetworkGUI(11100));
+		Player p3 = new Player(Faction.RED, new NetworkGUI(11101));
 		
-		final GameClient client = new GameClient(11100, "127.0.0.1", new TestGUI(Color.BLACK));
+		final GameClient client = new GameClient(11100, "127.0.0.1", new TestGUI(Color.BLACK)); 
+		final GameClient client2 = new GameClient(11101, "127.0.0.1", new TestGUI(Color.RED)); 
+		
+		Player[] list = {p1, p2, p3};
 		
 		Thread t = new Thread() {
 	        public void run () {
@@ -40,7 +44,19 @@ public class NetworkingTester {
 	        	}
 	        }
 	    };
-	    t.start();	
+	    t.start();
+	    
+		Thread t2 = new Thread() {
+	        public void run () {
+	        	
+	        	client2.connect();
+	        	while(true)
+	        	{
+	        		client2.read();
+	        	}
+	        }
+	    };
+	    t2.start();
 	    
 	    GameState state = new GameState(list, new Board(), new TestDeck(), new StandardTreasureBag(),
 	    		new StandardScoreCounter());
